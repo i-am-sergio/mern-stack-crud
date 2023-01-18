bb31b99a3d145be338af9a20c83615e0f77b41de
sergio15dev <sergio15dev@gmail.com>
2023-01-17 19:32:33 -0500

    package.json modified

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,13 +6,23 @@ import { PostCard } from "../components/PostCard";
 export function HomePage() {
   const { posts } = usePosts();
 
-  if (posts.length === 0)
+  const renderMain = () => {
+    if (posts.length === 0)
+      return (
+        <div className="flex flex-col justify-center items-center">
+          <VscEmptyWindow className="w-48 h-48 text-white" />
+          <h1 className="text-white text-2xl">There are no posts</h1>
+        </div>
+      );
+
     return (
-      <div className="flex flex-col justify-center items-center">
-        <VscEmptyWindow className="w-48 h-48 text-white" />
-        <h1 className="text-white text-2xl">There are no posts</h1>
+      <div className="grid grid-cols-3 gap-2">
+        {posts.map((post) => (
+          <PostCard key={post._id} post={post} />
+        ))}
       </div>
     );
+  };
 
   return (
     <div className="text-white pt-12 pb-4">
@@ -27,12 +37,7 @@ export function HomePage() {
           Create New Post
         </Link>
       </header>
-
-      <div className="grid grid-cols-3 gap-2">
-        {posts.map((post) => (
-          <PostCard key={post._id} post={post} />
-        ))}
-      </div>
-    </div>
+      {renderMain()}
+    </div> 
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import fileUpload from "express-fileupload";
 import postsRoutes from "./routes/posts.routes.js";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 // import cors from 'cors'
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // middlewares
 // app.use(cors())
@@ -17,5 +20,7 @@ app.use(
 
 // routes
 app.use(postsRoutes);
+console.log(__dirname);
+app.use(express.static(join(__dirname, "../client/build")));
 
 export default app;
